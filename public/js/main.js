1568d03bf9c45f2f2e0df8016e5443044056d4f6
Eslam Wael <84073355+SolomDev00@users.noreply.github.com>
2023-05-25 17:10:14 +0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -64,3 +64,19 @@ function toggle(div_id) {
     toggle("blanket");
     toggle(windowname);
   }
+
+let bgLoading = document.getElementById("bgLoading");
+let bgLoadingCont = document.querySelector("#bgLoading .loader");
+window.onload = () => {
+  setTimeout(function () {
+    bgLoadingCont.style.opacity = "0";
+    bgLoading.style.backgroundColor = "transparent";
+    bgLoading.style.pointerEvents = "none";
+    bgLoading.style.backdropFilter = "blur(0px)";
+    setTimeout(function () {
+      bgLoadingCont.style.display = "none";
+      bgLoading.style.display = "none";
+    }, 2500);
+  }, 3000);
+};
+
